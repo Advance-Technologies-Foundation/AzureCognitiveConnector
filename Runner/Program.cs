900287920889c4af1786efde34c769337d9fc117
alexbondj <alexbondj@gmail.com>
2017-12-14 14:56:48 +0200

    Free user request limitations

--- Runner/Program.cs
+++ Runner/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TagExtractor;
 
 namespace Runner
@@ -13,10 +9,14 @@ namespace Runner
 		private static string _defaultUrl = "https://westeurope.api.cognitive.microsoft.com/text/analytics/v2.0/";
 
 		static void Main(string[] args) {
-			var tExtractor = new TagExtractorExecuter(_defaultUrl, _defaultApiKey);
-			tExtractor.GetTags(new Guid(), @"The entire Pro Git book, written by Scott Chacon and Ben Straub and published by Apress, is available here.
+			var tExtractor = new TagExtractorExecuter(_defaultUrl, _defaultApiKey, s => s.Equals(_defaultApiKey) );
+			try {
+				tExtractor.GetTags(new Guid(), @"The entire Pro Git book, written by Scott Chacon and Ben Straub and published by Apress, is available here.
 				All content is licensed under the Creative Commons Attribution Non Commercial Share Alike 3.0 license. 
 				Print versions of the book are available on Amazon.com.").ForEach(Console.WriteLine);
+			} catch (Exception ex) {
+				Console.WriteLine(ex.Message);
+			}
 			try {
 				var	tags = tExtractor.GetTags(new Guid(), @"Что то на русском");
 				tags.ForEach(Console.WriteLine);
--- TagExtractor/TagExtractor.cs
+++ TagExtractor/TagExtractor.cs
@@ -7,20 +7,36 @@ namespace TagExtractor
 {
 	public class TagExtractorExecuter
 	{
+		private const string DemoKey = "78fd64a2f2474316a341ead6ce0d198e";
 
-		public TagExtractorExecuter(string baseUrl, string apiKey) {
+		private readonly ILangExtractor _langExtractor;
+		private readonly IKeyPhrasesExtractor _keyPhrasesExtractor;
+		private Predicate<string> _checkLic;
+		private readonly bool _isDemoMode;
+
+		public TagExtractorExecuter(string baseUrl, string apiKey, Predicate<string> checkLic = null) {
+			_checkLic = checkLic;
+			_isDemoMode = apiKey.Equals(DemoKey);
 			_langExtractor = new AzureHttpLangExtractor(baseUrl, apiKey);
 			_keyPhrasesExtractor = new AzureHttpKeyPhrasesExtractor(baseUrl, apiKey);
 		}
 
-		private ILangExtractor _langExtractor;
-		private IKeyPhrasesExtractor _keyPhrasesExtractor;
+		private void CheckDemoMode() {
+			if (!_isDemoMode) return;
+			if (_checkLic == null) {
+				throw new DemoModeException("You can't use this app in demo mode! Please register acount on https://portal.azure.com.");
+			}
+			if (!_checkLic(DemoKey)) {
+				throw new DemoModeException("You have exceeded the number of requests in demo mode! Please register acount on https://portal.azure.com.");
+			}
+		}
 
 		private async Task<string> AsyncGetLangByText(Guid id, string text) {
 			return await _langExtractor.AsyncDetectLang(id, text);
 		}
 
 		public List<string> GetTags(Guid recordId, string text, string lang) {
+			CheckDemoMode();
 			try {
 				var result = _keyPhrasesExtractor.AsyncKeyPhrases(lang, recordId, text).GetAwaiter();
 				return result.GetResult();
@@ -30,6 +46,7 @@ namespace TagExtractor
 		}
 
 		public List<string> GetTags(Guid recordId, string text) {
+			CheckDemoMode();
 			var pText = HttpUtility.HtmlEncode(text);
 			var lang = AsyncGetLangByText(recordId, pText).Result;
 			return GetTags(recordId, pText, lang);
